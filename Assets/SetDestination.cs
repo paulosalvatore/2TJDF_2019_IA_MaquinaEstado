125ce6b76767c85277d66c20a68954c801f647fa
Paulo Salvatore <paulo@salvatore.tech>
2019-08-06 12:09:35 -0300

    Initial commit

---
+++ Assets/SetDestination.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetDestination : MonoBehaviour
+{
+    public Transform target;
+
+    public NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        navMeshAgent.destination = target.position;
+    }
+}
